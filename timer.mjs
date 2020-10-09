4090d7792594ebe97bd3130f0c36ef0f98c2ddb4
leafs1 <michael.adragna1@gmail.com>
2020-10-09 18:48:32 -0400

    writeByte function for timer created

--- timer.mjs
+++ timer.mjs
@@ -16,6 +16,18 @@ TIMER = {
             return TIMER.tac
         }
     },
+
+    wb: function(addr, val) {
+        if (addr == 0xff04) {
+            TIMER.div = 0
+        } else if (addr == 0xff05) {
+            TIMER.tima = val
+        } else if (addr == 0xff06) {
+            TIMER.tma = val
+        } else if (addr == 0xff07) {
+            TIMER.tac = val & 7
+        }
+    }
 }
 
 export default TIMER
